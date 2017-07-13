ce8b0f7c758856bd49eb7465a20f55ae5a0d2ddd
Ilya Kovalenko <ilyacoding@yandex.com>
2017-07-13 19:12:20 +0300

    commited

---
+++ test/system/manuals_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ManualsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit manuals_url
+  #
+  #   assert_selector "h1", text: "Manual"
+  # end
+end
