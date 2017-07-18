3253d69be9dcb5424f6d6ccc01b1cba8b6746283
Ilya Kovalenko <ilyacoding@yandex.com>
2017-07-19 01:54:12 +0300

    added cancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :manuals
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'manuals#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :manuals do
+    resources :pages
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'manuals#index'
 end
