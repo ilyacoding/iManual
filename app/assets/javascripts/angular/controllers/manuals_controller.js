c592fb9e7b179a0b475ca01e8fac9628bcca0e4f
Ilya Kovalenko <ilyacoding@yandex.com>
2017-07-23 01:39:25 +0300

    need to implement resources

---
+++ app/assets/javascripts/angular/controllers/manuals_controller.js
@@ -0,0 +1,2 @@
+var app = angular.module('manualApp', ['ngRoute', 'ngResource', 'ui.sortable'])
+    .controller('PageListCtrl', ['$scope', PageListCtrl]);
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, :unless => :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
@@ -29,4 +30,19 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+  # override the devise method for where to go after signing out because theirs
+  # always goes to the root path. Because devise uses a session variable and
+  # the session is destroyed on log out, we need to use request.referrer
+  # root_path is there as a backup
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
+  end
 end
