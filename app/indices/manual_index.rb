942ff96e779891da2e299378fb5226a7604087ba
Ilya Kovalenko <ilyacoding@yandex.com>
2017-08-05 14:41:35 +0300

    feature/sphinx search

--- app/indices/manual_index.rb
+++ app/indices/manual_index.rb
@@ -1,3 +1,12 @@
+ThinkingSphinx::Index.define :manual, :with => :real_time do
+  indexes name, :type => :string, :sortable => true
 
-class 
+  # indexes content, :type => :text
+  # indexes user.name, :as => :user, :sortable => true
+  #
+  # has tag_ids, :multi => true
+  has id, :as => :manual_id, :type => :integer
+  has user_id,  :type => :integer
+  has created_at, :type => :timestamp
+  has updated_at, :type => :timestamp
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   resourcify
   validates :content, presence: true, length: {minimum: 3, maximum: 2000}
+  after_create_commit { CommentBroadcastJob.perform_later(self) }
+  after_save ThinkingSphinx::RealTime.callback_for(:manual, [:manual])
   belongs_to :user
   belongs_to :manual, counter_cache: true
-  after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
