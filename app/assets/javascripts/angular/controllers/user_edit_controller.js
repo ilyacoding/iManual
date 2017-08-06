afd6d3d1b29d86586947cc1895835ebc6a9eb254
Ilya Kovalenko <ilyacoding@yandex.com>
2017-08-06 18:09:26 +0300

    fix/profile edit

--- app/assets/javascripts/angular/controllers/user_edit_controller.js
+++ app/assets/javascripts/angular/controllers/user_edit_controller.js
@@ -1,14 +1,20 @@
 angular.module('app').controller('UserEditCtrl', ['$scope', '$http', '$location', 'Users', 'User', function ($scope, $http, $location, Users, User)
 {
-    $scope.getUser = function(userId) {
+    $scope.form = {
+        state: {},
+        data: {}
+    };
+
+    $scope.initializeUser = function(userId) {
         $scope.user = User.get({id: userId});
+    };
+
+    $scope.saveForm = function () {
+        $scope.updateUser();
     };
 
     $scope.updateUser = function () {
         User.update({ id: $scope.user.id }, $scope.user, function (response) {});
     };
 
-    $scope.cl = function () {
-
-    };
 }]);
