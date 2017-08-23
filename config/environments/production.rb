810df0a2ca4299d09c78bd3d3294a4514d9f0dbe
Ilya Kovalenko <ilyacoding@yandex.com>
2017-08-24 00:08:55 +0300

    fix/heroku actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_cable.url = "ws://imanual.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [
+      'https://imanual.herokuapp.com', 'http://imanual.herokuapp.com']
 end
