9412bd35b691128c0444798ddf6f179428fe7937
Ilya Kovalenko <ilyacoding@yandex.com>
2017-11-17 01:23:37 +0300

    Initial v2

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -1,10 +1,10 @@
 class Manual < ApplicationRecord
   resourcify
   acts_as_taggable
-  ratyrate_rateable "rating"
+  ratyrate_rateable 'rating'
   validates :name, presence: true
-  belongs_to :user, :counter_cache => true
-  belongs_to :category, :counter_cache => true
+  belongs_to :user, counter_cache: true
+  belongs_to :category, counter_cache: true
   has_many :steps, dependent: :destroy
   has_many :comments, dependent: :destroy
   after_save ThinkingSphinx::RealTime.callback_for(:manual)
