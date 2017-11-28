9f536f70259d8a45fbcd590a6af0a2de66e490c4
Ilya Kovalenko <ilyacoding@yandex.com>
2017-11-29 02:48:07 +0300

    Fix local real-time comments support

--- app/assets/javascripts/channels/manuals.js
+++ app/assets/javascripts/channels/manuals.js
@@ -1,29 +1,25 @@
-
 //= require action_cable
 //= require_self
 //= require_tree .
 
-// $(function () {
-//     this.App = {};
-//     var comments = $('#comments');
-//     // if (comments.data('id') != null)
-//     // {
-//         App.cable = ActionCable.createConsumer();
-//         App.global_manual = App.cable.subscriptions.create({
-//             channel: "CommentsChannel",
-//             manual_id: comments.data('id')
-//         },
-//         {
-//             connected: function() {},
-//             disconnected: function() {},
-//             received: function(data) {
-//                 comments.append(data['comment']);
-//                 comments.children("div").last().effect("highlight", {}, 2000);
-//             }
-//         });
-//
-//         $("#new_comment").bind("ajax:complete", function(event,xhr,status){
-//             $('#comment_content').val('');
-//         });
-//     // }
-// });
+$(function () {
+    var comments = $('#comments');
+    if (comments.data('id') != null) {
+        var App = {};
+        App.cable = ActionCable.createConsumer();
+        App.global_manual = App.cable.subscriptions.create({
+            channel: "CommentsChannel",
+            manual_id: comments.data('id')
+        },
+        {
+            received: function(data) {
+                comments.append(data['comment']);
+                comments.children("div").last().effect("highlight", {}, 2000);
+            }
+        });
+
+        $("#new_comment").bind("ajax:complete", function(event,xhr,status) {
+            $('#comment_content').val('');
+        });
+    }
+});
