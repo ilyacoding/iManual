d39204eedcc5eb9595dd711c47286cfee82c4d98
Ilya Kovalenko <ilyacoding@yandex.com>
2018-03-05 01:07:26 +0300

    OmniauthCallbacksController refactoring

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,38 +1,35 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    process_auth "facebook"
-  end
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def facebook
+      process_auth("facebook")
+    end
 
-  def vk
-    process_auth "vk"
-  end
+    def vk
+      process_auth("vk")
+    end
 
-  def twitter
-    process_auth "twitter"
-  end
+    def twitter
+      process_auth("twitter")
+    end
 
-  def google_oauth2
-    process_auth "google"
-  end
+    def google_oauth2
+      process_auth("google")
+    end
 
-  def after_sign_in_path_for(resource)
-    super resource
-  end
+    def failure
+      redirect_to(root_path)
+    end
 
-  def failure
-    redirect_to root_path
-  end
+    private
 
-  private
-
-  def process_auth(provider)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-    else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+    def process_auth(provider)
+      if (user = User.from_omniauth(request.env["omniauth.auth"])).persisted?
+        sign_in_and_redirect(user, event: :authentication)
+        set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
+      else
+        session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+        redirect_to(new_user_registration_url)
+      end
     end
   end
 end
